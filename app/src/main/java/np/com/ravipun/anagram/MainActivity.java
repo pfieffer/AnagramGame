bffdf81cb8b05eff73bac000114ffa297a6112df
RaviGarbujaPun <cbpmgr@gmail.com>
2017-09-09 13:05:27 +0545

    Primitive level up mechanism impemented

--- app/src/main/java/np/com/ravipun/anagram/MainActivity.java
+++ app/src/main/java/np/com/ravipun/anagram/MainActivity.java
@@ -20,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
     EditText guessedWord;
     Button guess, newWord;
     int WordIdx=0;
-    TextView scrambledWord, feedback;
+    TextView scrambledWord, feedback, levelIndicator;
 
     MediaPlayer mp;
+
+    int points=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +36,10 @@ public class MainActivity extends AppCompatActivity {
         newWord = (Button) findViewById(R.id.button_new_word);
         guess = (Button) findViewById(R.id.button_guess);
         feedback = (TextView) findViewById(R.id.feedback_area);
+        levelIndicator = (TextView) findViewById(R.id.level_indicator);
 
         generateRandomScrambledWord();
-  
+
         guess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                     mp  = MediaPlayer.create(MainActivity.this, R.raw.correct);//for sounds
                     mp.start();
                     feedback.setText(R.string.correct_ans);
+                    points++;
                 }
                 else {
                     mp  = MediaPlayer.create(MainActivity.this,R.raw.wrong);
@@ -67,7 +71,30 @@ public class MainActivity extends AppCompatActivity {
 
     private void generateRandomScrambledWord() {
         Random rand = new Random();
-        int  n = rand.nextInt(44) + 0;  //generate random number from 0 to 44; 45 is the number of words in out StaticWordLibrary
+        int n=0;
+        if(points<5){
+            n = rand.nextInt(8) + 0;  //generate random number from 0 to 8; 45 is the number of words in out StaticWordLibrary
+        }else if (points>=5 && points<10){
+            n = rand.nextInt(17) + 9;
+            levelIndicator.setText("You are now level 2");
+            mp  = MediaPlayer.create(MainActivity.this, R.raw.level_up);//for sounds
+            mp.start();
+        } else if (points>=10 && points<15){
+            n = rand.nextInt(26) + 18;
+            levelIndicator.setText("You are now level 3");
+            mp  = MediaPlayer.create(MainActivity.this, R.raw.level_up);//for sounds
+            mp.start();
+        } else if (points>=15 && points<20){
+            n = rand.nextInt(35) + 27;
+            levelIndicator.setText("You are now level 4");
+            mp  = MediaPlayer.create(MainActivity.this, R.raw.level_up);//for sounds
+            mp.start();
+        } else if (points>=20 && points<25){
+            n = rand.nextInt(44) + 36;
+            levelIndicator.setText("You are now level 5");
+            mp  = MediaPlayer.create(MainActivity.this, R.raw.level_up);//for sounds
+            mp.start();
+        }
         scrambledWord.setText(StaticWordLibrary.getDefault().getScrambledWord(n));
         WordIdx=n;
     }
